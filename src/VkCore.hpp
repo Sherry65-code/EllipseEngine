c4f8fbc76364c4f04080af270a4c113fbbe0bfb0
Sherry65-code <sparambir65@gmail.com>
2024-05-17 20:36:41 +0530

    High Performance Memory! BOOOM!!!:

--- src/VkCore.hpp
+++ src/VkCore.hpp
@@ -83,6 +83,8 @@ class Core {
     void recreateSwapChain();
     void cleanupSwapChain();
     uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
+    void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
+    void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
 
     public:
 
